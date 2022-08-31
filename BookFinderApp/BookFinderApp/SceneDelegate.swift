00f9d435e59cd4ea1160354a8826c11b964f23e0
k-nh <skgml977@gmail.com>
2022-09-01 02:32:50 +0900

    [추가] 프로젝트 생성 및 세팅

---
+++ BookFinderApp/BookFinderApp/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  BookFinderApp
+//
+//  Created by 김나희 on 9/1/22.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        
+        let rootViewController = ViewController()
+        window?.rootViewController = UINavigationController(rootViewController: rootViewController)
+        window?.makeKeyAndVisible()
+    }
+
+}
+
