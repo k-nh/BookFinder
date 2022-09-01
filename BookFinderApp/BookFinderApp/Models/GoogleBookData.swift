53da1f44d9928f68aa6fcd8302d6c7170e594132
k-nh <skgml977@gmail.com>
2022-09-02 02:40:14 +0900

    [수정] 네트워크 모델 수정

--- BookFinderApp/BookFinderApp/Models/GoogleBookData.swift
+++ BookFinderApp/BookFinderApp/Models/GoogleBookData.swift
@@ -8,81 +8,140 @@
 import Foundation
 
 // MARK: - GoogleBookData
-struct GoogleBookData {
-    let id: String
-    let kind, etag: String
-    let selfLink: String
-    let volumeInfo: VolumeInfo
-    let saleInfo: SaleInfo
-    let accessInfo: AccessInfo
+struct GoogleBookData: Codable {
+    let kind: String?
+    let totalItems: Int?
+    let items: [Item]?
+}
+
+// MARK: - Item
+struct Item: Codable {
+    let kind, id, etag: String?
+    let selfLink: String?
+    let volumeInfo: VolumeInfo?
+    let saleInfo: SaleInfo?
+    let accessInfo: AccessInfo?
+    let searchInfo: SearchInfo?
 }
 
 // MARK: - AccessInfo
-struct AccessInfo {
-    let country, viewability: String
-    let embeddable, publicDomain: Bool
-    let textToSpeechPermission: String
-    let epub: Epub
-    let pdf: PDF
-    let accessViewStatus: String
+struct AccessInfo: Codable {
+    let country, viewability: String?
+    let embeddable, publicDomain: Bool?
+    let textToSpeechPermission: String?
+    let epub: Epub?
+    let pdf: PDF?
+    let webReaderLink: String?
+    let accessViewStatus: String?
+    let quoteSharingAllowed: Bool?
 }
 
 // MARK: - Epub
-struct Epub {
-    let isAvailable: Bool
-    let acsTokenLink: String
+struct Epub: Codable {
+    let isAvailable: Bool?
 }
 
 // MARK: - PDF
-struct PDF {
-    let isAvailable: Bool
+struct PDF: Codable {
+    let isAvailable: Bool?
+    let acsTokenLink: String?
 }
 
 // MARK: - SaleInfo
-struct SaleInfo {
-    let country, saleability: String
-    let isEbook: Bool
-    let listPrice, retailPrice: Price
-    let buyLink: String
+struct SaleInfo: Codable {
+    let country, saleability: String?
+    let isEbook: Bool?
+    let listPrice, retailPrice: SaleInfoListPrice?
+    let buyLink: String?
+    let offers: [Offer]?
 }
 
-// MARK: - Price
-struct Price {
-    let amount: Double
-    let currencyCode: String
+// MARK: - SaleInfoListPrice
+struct SaleInfoListPrice: Codable {
+    let amount: Int?
+    let currencyCode: String?
+}
+
+// MARK: - Offer
+struct Offer: Codable {
+    let finskyOfferType: Int?
+    let listPrice, retailPrice: OfferListPrice?
+}
+
+// MARK: - OfferListPrice
+struct OfferListPrice: Codable {
+    let amountInMicros: Int?
+    let currencyCode: String?
+}
+
+// MARK: - SearchInfo
+struct SearchInfo: Codable {
+    let textSnippet: String?
 }
 
 // MARK: - VolumeInfo
-struct VolumeInfo {
-    let title: String
-    let authors: [String]
-    let publisher, publishedDate, volumeInfoDescription: String
-    let industryIdentifiers: [IndustryIdentifier]
-    let pageCount: Int
-    let dimensions: Dimensions
-    let printType, mainCategory: String
-    let categories: [String]
-    let averageRating: Double
-    let ratingsCount: Int
-    let contentVersion: String
-    let imageLinks: ImageLinks
-    let language: String
-    let infoLink: String
-    let canonicalVolumeLink: String
-}
+struct VolumeInfo: Codable {
+    let title: String?
+    let authors: [String]?
+    let publisher, volumeInfoDescription: String?
+    let industryIdentifiers: [IndustryIdentifier]?
+    let readingModes: ReadingModes?
+    let pageCount: Int?
+    let printType: String?
+    let categories: [String]?
+    let maturityRating: String?
+    let allowAnonLogging: Bool?
+    let contentVersion: String?
+    let panelizationSummary: PanelizationSummary?
+    let comicsContent: Bool?
+    let imageLinks: ImageLinks?
+    let language: String?
+    let previewLink: String?
+    let infoLink: String?
+    let canonicalVolumeLink: String?
+    let seriesInfo: SeriesInfo?
+    let publishedDate: String?
 
-// MARK: - Dimensions
-struct Dimensions {
-    let height, width, thickness: String
+    enum CodingKeys: String, CodingKey {
+        case title, authors, publisher
+        case volumeInfoDescription = "description"
+        case industryIdentifiers, readingModes, pageCount, printType, categories, maturityRating, allowAnonLogging, contentVersion, panelizationSummary, comicsContent, imageLinks, language, previewLink, infoLink, canonicalVolumeLink, seriesInfo, publishedDate
+    }
 }
 
 // MARK: - ImageLinks
-struct ImageLinks {
-    let smallThumbnail, thumbnail, small, medium: String
-    let large, extraLarge: String
+struct ImageLinks: Codable {
+    let smallThumbnail, thumbnail: String?
 }
 
 // MARK: - IndustryIdentifier
-struct IndustryIdentifier {
-    let type, identifier: String
+struct IndustryIdentifier: Codable {
+    let type, identifier: String?
+}
+
+// MARK: - PanelizationSummary
+struct PanelizationSummary: Codable {
+    let containsEpubBubbles, containsImageBubbles: Bool?
+}
+
+// MARK: - ReadingModes
+struct ReadingModes: Codable {
+    let text, image: Bool?
+}
+
+// MARK: - SeriesInfo
+struct SeriesInfo: Codable {
+    let kind, bookDisplayNumber: String?
+    let volumeSeries: [VolumeSery]?
+}
+
+// MARK: - VolumeSery
+struct VolumeSery: Codable {
+    let seriesID, seriesBookType: String?
+    let orderNumber: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case seriesID = "seriesId"
+        case seriesBookType, orderNumber
+    }
 }
